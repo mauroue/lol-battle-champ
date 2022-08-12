57d85a5d4467c32863ffb0bcb62a23a08d683b4c
Mauro Ue <mauroue@gmail.com>
2022-08-11 22:40:40 -0300

    basic ui layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,15 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className='text-center'>hello world</h1>
+      <h1 className='h-screen w-screen flex flex-col justify-center'>
+        <div className='text-2xl text-center'>Who wins?</div>
+        <div className='p-6'></div>
+        <div className='border rounded p-6 flex justify-between items-center max-w-2xl'>
+          <div className='bg-red-400 w-32 h-32'></div>
+          <div className='p-8'>vs</div>
+          <div className='bg-red-200 w-32 h-32'></div>
+        </div>
+      </h1>
     </>
   );
 };
