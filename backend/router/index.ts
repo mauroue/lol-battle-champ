e9f47af49c812e2c66b4af25976f0c19cb9fb737
Mauro Ue <mauroue@gmail.com>
2022-08-16 15:29:50 -0300

    added prisma and basic voting endpoint

--- backend/router/index.ts
+++ backend/router/index.ts
@@ -1,17 +1,29 @@
-import data from "../../utils/data.json";
-import * as trpc from "@trpc/server";
-import { z } from "zod";
+import data from '../../utils/data.json';
+import * as trpc from '@trpc/server';
+import { z } from 'zod';
+import { prisma } from '../utils/prisma';
 
-export const appRouter = trpc.router().query("get-champ-by-id", {
-  input: z.object({
-    id: z.number(),
-  }),
-  async resolve({ input }) {
-    const championData = data.find((champion) => champion.id === input.id);
-
-    return championData;
-  },
-});
+export const appRouter = trpc
+  .router()
+  .query('get-champ-by-id', {
+    input: z.object({
+      id: z.number()
+    }),
+    async resolve({ input }) {
+      const championData = data.find((champion) => champion.id === input.id);
+      return championData;
+    }
+  })
+  .mutation('cast-vote', {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number()
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({ data: { ...input } });
+      return { sucess: true };
+    }
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
